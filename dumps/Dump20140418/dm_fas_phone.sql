2a4247e0cfffe01bf99b94b730cee51cf9cf9e86
vincentclee <vincentlee.dolbydigital@yahoo.com>
2014-04-21 10:55:31 -0500

    SQL Dumps Added

---
+++ dumps/Dump20140418/dm_fas_phone.sql
@@ -0,0 +1,62 @@
+CREATE DATABASE  IF NOT EXISTS `dm` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `dm`;
+-- MySQL dump 10.13  Distrib 5.1.73, for redhat-linux-gnu (x86_64)
+--
+-- Host: 127.0.0.1    Database: dm
+-- ------------------------------------------------------
+-- Server version	5.1.73
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `fas_phone`
+--
+
+DROP TABLE IF EXISTS `fas_phone`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fas_phone` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `fas_id` int(10) unsigned DEFAULT NULL,
+  `fas_address_id` int(10) unsigned DEFAULT NULL,
+  `country_code` varchar(6) DEFAULT NULL,
+  `area_code` varchar(6) DEFAULT NULL,
+  `exchange_code` varchar(6) DEFAULT NULL,
+  `line_number` varchar(6) DEFAULT NULL,
+  `extension` varchar(6) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_fas_phone_1_idx` (`fas_id`),
+  KEY `fk_fas_phone_2_idx` (`fas_address_id`),
+  CONSTRAINT `fk_fas_phone_1` FOREIGN KEY (`fas_id`) REFERENCES `faculty_and_staff` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `fk_fas_phone_2` FOREIGN KEY (`fas_address_id`) REFERENCES `fas_address` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fas_phone`
+--
+
+LOCK TABLES `fas_phone` WRITE;
+/*!40000 ALTER TABLE `fas_phone` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fas_phone` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-04-18 15:37:18
